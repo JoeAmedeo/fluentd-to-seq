9a642818869a2b7a5db8f0fa68b81430d5fb04f6
joeamedeo <joseph_amedeo@yahoo.com>
2020-08-14 23:27:32 -0400

    working on styling and dark theme

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
-function App() {
+const App = () => {
+
+  
+
+
   return (
     <div className="app">
-      <header className="title">
-        Send some logs!
-      </header>
-      <div className="form">
-        <input className="textInput" type="text" id="textInput" placeholder="Type a message to log..."/>
-        <select className="logType" id="logType">
-          <option value="info">Info</option>
-          <option value="warn">Warn</option>
-          <option value="error">Error</option>
-        </select>
-        <button className="sendButton">Send</button>
+      <div className="panel">
+        <header className="title">
+          Send some logs!
+        </header>
+        <div className="form">
+          <input className="textInput" type="text" id="textInput" placeholder="Type a message to log..."/>
+          <select className="logType" id="logType">
+            <option value="info">Info</option>
+            <option value="warn">Warn</option>
+            <option value="error">Error</option>
+          </select>
+          <button className="sendButton">Send</button>
+        </div>
       </div>
     </div>
   );
